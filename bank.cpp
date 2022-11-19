57b4227e4c41fde0c779cce9866dbf70309c26d7
manju sree <101690285+Manju-Sree-BB6@users.noreply.github.com>
2022-11-19 14:39:58 +0530

    start

---
+++ bank.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+{
+	int fd,age,ta;
+	printf("enter fixed deposit:");
+	scanf("%d",&fd);
+	printf("enter age:");
+	scanf("%d",&age);
+	if(fd<=10000)
+	if(age>60)
+	{
+		ta==fd+6*100/100;
+	}
+	else
+	{
+		ta=fd+5*100/100;
+	}
+	if(fd<=20000)
+	if(age>60)
+	{
+		ta=fd+7*100/100;
+	}
+	else
+	{
+		ta=fd+6*100/100;
+	}
+	if(fd<=10000)
+	if(age>60)
+	{
+		ta=fd+8*100/100;
+	}
+	else
+	{
+		ta=fd+7*100/100;
+	}
+	printf("ta=%d",ta);
+}
